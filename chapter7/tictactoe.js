b4caf47a24b6e5705993a080655baa7975b00df3
sch <73852281+ChangHui-Song@users.noreply.github.com>
2022-07-09 21:26:33 +0900

    틱택토 구현 완료

---
+++ chapter7/tictactoe.js
@@ -0,0 +1,81 @@
+const $table = document.createElement('table');
+const $result = document.createElement('div');
+const board = [];
+let turn = 'O';
+for(let i = 0; i < 3; i++) {
+  board.push([]);
+}
+
+document.body.append($table);
+document.body.append($result);
+
+const checkWinner = (target) => {
+  const rowIndex = target.parentNode.rowIndex;
+  const cellIndex = target.cellIndex;
+  let hasWinner = false;
+  
+  if (
+    board[rowIndex][0].textContent === turn &&
+    board[rowIndex][1].textContent === turn &&
+    board[rowIndex][2].textContent === turn
+  ) {
+    hasWinner = true;
+  }
+  if (
+    board[0][cellIndex].textContent === turn &&
+    board[1][cellIndex].textContent === turn &&
+    board[2][cellIndex].textContent === turn 
+  ) {
+    hasWinner = true;
+  }
+  if (
+    board[0][0].textContent === turn &&
+    board[1][1].textContent === turn &&
+    board[2][2].textContent === turn
+  ) {
+    hasWinner = true;
+  }
+  if (
+    board[2][0].textContent === turn &&
+    board[1][1].textContent === turn &&
+    board[0][2].textContent === turn
+  ) {
+    hasWinner = true;
+  }
+  return hasWinner;
+}
+
+const callback = (event) => {
+  if ($result.textContent) {
+    return ;
+  }
+  if (event.target.textContent) return ;
+  event.target.textContent = turn;
+  if (checkWinner(event.target)) {
+    $result.textContent = `${turn}님의 승리!`;
+    return ;
+  }
+  let draw = true;
+  draw = board.flat().every((cell) => cell.textContent);
+  if (draw) {
+    $result.textContent = '무승부!';
+    return ;
+  }
+  // turn = turn === 'X' ? 'O' : 'X';
+  if (turn === 'O') {
+    turn = 'X';
+  } else if (turn === 'X') {
+    turn = 'O';
+  }
+};
+
+for (let i = 0; i < board.length; i++) {
+  const $tr = document.createElement('tr');
+  $table.append($tr);
+  for (let j = 0; j < board.length; j++) {
+    const $td = document.createElement('td');
+    $tr.append($td);
+    board[i].push($td);
+  }
+  $table.addEventListener('click', callback);
+}
