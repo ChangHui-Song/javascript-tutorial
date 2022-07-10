d6892a6e1b82768a6c89f88f7d32be7157b5cd66
sch <73852281+ChangHui-Song@users.noreply.github.com>
2022-07-11 01:01:00 +0900

    틱택토 컴퓨터 차례 딜레이 추가

--- chapter7/tictactoe.js
+++ chapter7/tictactoe.js
@@ -46,54 +46,37 @@ const checkWinner = (target) => {
 }
 
 const checkWinnerAndDraw = (target) => {
-  const hasWinner = checkWinner(randomCell);
+  const hasWinner = checkWinner(target);
   if (hasWinner) {
     $result.textContent = `${turn}님의 승리!`;
+    return true;
   }
   const draw = board.flat().every(cell => cell.textContent);
   if (draw) {
     $result.textContent = '무승부';
-    return ;
+    return true;
   }
   turn = turn === 'X' ? 'O' : 'X';
+  return false;
 }
 
+let clickable = true;
 const callback = (event) => {
-  if ($result.textContent) {
+  if ($result.textContent || !clickable) {
     return ;
   }
   if (event.target.textContent) return ;
   event.target.textContent = turn;
-  if (checkWinner(event.target)) {
-    $result.textContent = `${turn}님의 승리!`;
-    return ;
-  }
-  let draw = true;
-  draw = board.flat().every((cell) => cell.textContent);
-  if (draw) {
-    $result.textContent = '무승부!';
-    return ;
-  }
-  // turn = turn === 'X' ? 'O' : 'X';
-  if (turn === 'O') {
-    turn = 'X';
-  } else if (turn === 'X') {
-    turn = 'O';
-  }
+  if (checkWinnerAndDraw(event.target)) return ;
   if (turn === 'X') {
-    const emptyCells = board.flat().filter(v => !v.textContent);
-    const randomCell = emptyCells[Math.floor(Math.random() * emptyCells.length)];
-    randomCell.textContent = 'X';
-    const hasWinner = checkWinner(randomCell);
-    if (hasWinner) {
-      $result.textContent = `${turn}님의 승리!`;
-    }
-    const draw = board.flat().every(cell => cell.textContent);
-    if (draw) {
-      $result.textContent = '무승부';
-      return ;
-    }
-    turn = turn === 'X' ? 'O' : 'X';
+    clickable = false;
+    setTimeout(() => {
+      const emptyCells = board.flat().filter(v => !v.textContent);
+      const randomCell = emptyCells[Math.floor(Math.random() * emptyCells.length)];
+      randomCell.textContent = 'X';
+      if (checkWinnerAndDraw(randomCell)) return ;
+      clickable = true;
+    }, 1000);
   }
 };
 
