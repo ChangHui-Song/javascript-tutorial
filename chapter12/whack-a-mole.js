3508f90bd2de7b9c329b13db146d05dcf7c3408d
sch <73852281+ChangHui-Song@users.noreply.github.com>
2022-07-16 03:17:00 +0900

    화면 애니메이션 구현

--- chapter12/whack-a-mole.js
+++ chapter12/whack-a-mole.js
@@ -2,9 +2,27 @@ const $timer = document.querySelector('#timer');
 const $score = document.querySelector('#score');
 const $game = document.querySelector('#game');
 const $start = document.querySelector('#start');
-const $$cell = document.querySelectorAll('.cell');
+const $$cells = document.querySelectorAll('.cell');
 
+let holes = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 let started = false;
-$start.addEventListener('click', (event) => {
+let score = 0;
+
+const tick = () => {
+  holes.forEach((hole, index) => {
+    if (hole) return ;
+    const $gopher = $$cells[index].querySelector('.gopher');
+    holes[index] = setTimeout(() => {
+      $gopher.classList.add('hidden');
+      holes[index] = 0;
+    }, 1000);
+    $gopher.classList.remove('hidden');
+  });
+};
+
+$start.addEventListener('click', () => {
   if (started) return ;
-})
+  started = true;
+  const tickId = setInterval(tick, 1000);
+  tick();
+});
