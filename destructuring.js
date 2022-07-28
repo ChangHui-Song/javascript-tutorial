df39b3ba3aab9b7b202f69c21b6d1792ba2c6f8b
sch <73852281+ChangHui-Song@users.noreply.github.com>
2022-07-28 16:29:19 +0900

    destructuring assignment tutorial

---
+++ destructuring.js
@@ -0,0 +1,36 @@
+// destructuring assignment problem1
+let user = {
+  name: 'John',
+  years: 30
+};
+
+let {
+  name,
+  years: age,
+  isAdmin = false
+} = user;
+
+console.log(name, age, isAdmin);
+
+// destructuring assignment problem2
+let salaries = {
+  'John': 100,
+  'Pete': 300,
+  'Mary': 250,
+}
+
+console.log(topSalary(salaries));
+
+function topSalary(salaries) {
+  maxSalary = -1;
+  maxUser = null;
+
+  Object.entries(salaries).forEach(([name, salarie]) => {
+    if (maxSalary < salarie) {
+      maxSalary = salarie;
+      maxUser = name;
+    }
+  });
+
+  return maxUser;
+}
